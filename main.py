7909fdad3bf88b16cd944ecf34dd7cb04045ea32
Temirlan Mirulan <mirulanow@gmail.com>
2025-12-01 18:27:11 +0100

    format() allocate space

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ price2 = -723.12
 price3 = 12.42
 
 
-print(f"Price 1 is ${price1:010}") #Allocate space but with 0 pudded
-print(f"Price 2 is ${price2:010}")
-print(f"Price 3 is ${price3:010}")
+print(f"Price 1 is ${price1:>10}") # left justified by 10 spaces
+print(f"Price 2 is ${price2:<10}")
+print(f"Price 3 is ${price3:<10}")
