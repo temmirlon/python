6b341935d5ee40b918ec5b689c2d62aa5b49ec7a
Temirlan Mirulan <mirulanow@gmail.com>
2025-12-01 18:31:12 +0100

    format() allocate space

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-price1 = 3.12432
-price2 = -723.12
-price3 = 12.42
+price1 = 3000.12432
+price2 = -72333.12
+price3 = 11422.42
 
 
-print(f"Price 1 is ${price1:+}") # any positive number preceded with +
-print(f"Price 2 is ${price2:+}") # any negative number preceded with -
-print(f"Price 3 is ${price3:+}")
+print(f"Price 1 is ${price1:,}") # each thousands place is separated with ,
+print(f"Price 2 is ${price2:,}")
+print(f"Price 3 is ${price3:,}")
