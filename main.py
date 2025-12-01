aff0da99e1c89b313273be6eb08c43be2ac9ddc7
Temirlan Mirulan <mirulanow@gmail.com>
2025-12-01 18:29:55 +0100

    format() allocate space

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ price2 = -723.12
 price3 = 12.42
 
 
-print(f"Price 1 is ${price1:>10}") # left justified by 10 spaces
-print(f"Price 2 is ${price2:<10}")
-print(f"Price 3 is ${price3:<10}")
+print(f"Price 1 is ${price1:+}") # any positive number preceded with +
+print(f"Price 2 is ${price2:+}") # any negative number preceded with -
+print(f"Price 3 is ${price3:+}")
