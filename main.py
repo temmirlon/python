c0a6a0a16c910a9cc8a48eaa24c07d0033f6e873
temmirlon <mirulanow@gmail.com>
2025-12-02 15:23:59 +0100

    string -> strip()

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import re
-
-a = "apple orange apple banana"
-
-pattern = "apple"
-repl = "grape"
-
-# a way to modify strings by replacing specific patterns
-# Replace all occurrences of 'apple' with 'grape'
-result = re.sub(pattern, repl, a)
-
-print(result)
+# remove spaces at the beginning and at the end of the string
+a = "             apple orange apple banana         "
+print(a.strip())
